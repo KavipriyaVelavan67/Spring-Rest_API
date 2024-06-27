f21c117c116d0ecda2548a79e8bdb913dd5dcf66
Kavipriyavelavan67 <kavipriyavelavan149@gmail.com>
2024-06-27 18:31:54 +0530

    adding files

---
+++ src/main/java/com/Rest/API/model/Student.java
@@ -0,0 +1,39 @@
+package com.Rest.API.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//Data = @Getter, @Setter, @ToString, @EqualsAndHashCode and @RequiredArgsConstructor
+@Entity
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    Long id;
+
+    String name;
+
+    Integer age;
+
+    String regNo;
+
+    String contact;
+
+    Date dob;
+
+
+
+
+}
